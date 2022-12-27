63ebe4d11aaf11c2bd54744941ba48093b36abc8
Roman84asas <roma.mereneanu@mail.ru>
2022-12-27 17:31:19 +0200

    add

--- js/mine .js
+++ js/mine .js
@@ -15,4 +15,102 @@
         toggleHeader(document.querySelector('.menu-close'));
     }
 
+    if(document.querySelector('.slider-small-block_history_slider')) {
+        let historySlider = new Swiper(".slider-small-block_history_slider", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: '.history_left',
+                prevEl: '.history_right',
+            },
+            breakpoints: {
+                500: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                800: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1300: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+                1690: {
+                    slidesPerView: 5,
+                    spaceBetween: 30,
+                },
+                1920: {
+                    slidesPerView: 6,
+                    spaceBetween: 30,
+                },
+            }
+        });
+    }
+
+    if(document.querySelector('.slider-small-block_added_slider')) {
+        let addedSlider = new Swiper(".slider-small-block_added_slider", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: '.added_left',
+                prevEl: '.added_right',
+            },
+            breakpoints: {
+                500: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                800: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1300: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+                1690: {
+                    slidesPerView: 5,
+                    spaceBetween: 30,
+                },
+                1920: {
+                    slidesPerView: 6,
+                    spaceBetween: 30,
+                },
+            }
+        });
+    }
+
+    if(document.querySelector('.slider-small-block_liked_slider')) {
+        let likedSlider = new Swiper(".slider-small-block_liked_slider", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: '.liked_left',
+                prevEl: '.liked_right',
+            },
+            breakpoints: {
+                500: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                800: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1300: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1690: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+                1920: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+            }
+        });
+    }
 })();
